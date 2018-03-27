d191f2b393607f0e96694896d1b017c8d4a56600
Carie Pointer <cariepointer@yahoo.com>
2018-03-27 17:42:05 -0600

    Construct HashMap of Elements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
 package csci305.javalab;
+import java.util.*;
 
 public class Main {
+  private static final Map<String, Element> m = new HashMap<String, Element>();
 
   public static void main(String args[]) {
-
+    m.put("Rock", new Rock("Rock"));
+    m.put("Paper", new Paper("Paper"));
+    m.put("Scissors", new Rock("Scissors"));
+    m.put("Spock", new Rock("Spock"));
+    m.put("Lizard", new Rock("Lizard"));
   }
 }
