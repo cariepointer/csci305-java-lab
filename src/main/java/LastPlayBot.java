8eb8afeccea6cb06038f88d56cc57efe17971cf2
Carie Pointer <cariepointer@yahoo.com>
2018-03-28 22:03:29 -0600

    Add base methods

--- src/main/java/LastPlayBot.java
+++ src/main/java/LastPlayBot.java
@@ -2,5 +2,17 @@ package csci305.javalab;
 
 public class LastPlayBot extends Player {
 
-  
+  public LastPlayBot(String name) {
+    super(name);
+  }
+
+  public String getName() {
+    return super.getName();
+  }
+
+  @Override
+  public Element play() {
+    Element rock = new Rock("Rock");
+    return rock;
+  }
 }
